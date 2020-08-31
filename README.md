a1e9b111032fa8620077d981a92a9377e750d852
indahud <indahud@protonmail.com>
2020-08-31 18:41:53 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# crypto-price
+# Top 10 Cryptocurrency Prices, market cap and volume information.
+
+## Tools Used
+- ReactJS
+- CoinGecko API
+- Github Pages for deployment
+---
+[Deployed Url](https://indahud.github.io/cryptoprice/)
+[Code for repo](https://indahud.com/)
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import Coin from './Coin';
+
+function App() {
+  const [coins, setCoins ] = useState([]);
+  const [search, setSearch ] = useState('');
+  const [isError, setIsError ] = useState(false)
+  useEffect(() => {
+    // axios
+    //   .get(
+    //     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+    //   )
+    //   .then(res => {
+    //     setCoins(res.data);
+    //     console.log(res.data);
+    //   })
+    //   .catch(error => console.log(error));
+
+      // (async function getData() {
+      //   const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+      //   const d = await setCoins(res.data)
+      //   console.log(d)  
+      // })();
+
+      const fetchData = async() => {
+        try {
+          const fetchData = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+          setCoins(fetchData.data)
+        } catch (error) {
+          setIsError(true)
+        }
+      }
+      fetchData()
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
